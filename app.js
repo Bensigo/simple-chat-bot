00605c2027d4dd4afca9f41f4e038904bfcfeef4
larry sigo <sigocode@gmail.com>
2017-05-10 11:56:11 +0100

    change the port env on app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express'),
 
 
 //creating the port 
-const port = process.env.port ||4000
+const port = process.env.PORT ||4000
 
 //instancecating app
 const app = express();
